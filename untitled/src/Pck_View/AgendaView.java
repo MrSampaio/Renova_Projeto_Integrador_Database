13dc601a63777c5a660644556647b70b26135b08
MrSampaio <jusampa2@gmail.com>
2026-04-24 10:08:40 -0300

    fixes

--- untitled/src/Pck_View/AgendaView.java
+++ untitled/src/Pck_View/AgendaView.java
@@ -1,5 +1,37 @@
 package Pck_View;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
-public class AgendaView {
+public class AgendaView extends JFrame{
+    private static final long serialVersionUI = 1L;
+
+    JLabel texto;
+    JTextField caixaTexto;
+    JButton botaoInsert;
+
+    public AgendaView(){
+        setTitle("Gabriela cu de rodela");
+        setBounds(100, 100, 430, 300);
+
+        texto = new JLabel("Gabriela libera o Murilo");
+        texto.setBounds(20, 20, 100, 20);
+
+        caixaTexto = new JTextField("Informe o tamanho do penis do Murilo");
+        caixaTexto.setBounds(130, 20, 100, 20);
+
+        botaoInsert = new JButton("Enviar");
+        botaoInsert.setBounds(20, 80, 100, 20);
+
+        getContentPane().setLayout(null);
+        getContentPane().add(texto);
+        getContentPane().add(caixaTexto);
+        getContentPane().add(botaoInsert);
+
+
+    }
+
+
+
 }
