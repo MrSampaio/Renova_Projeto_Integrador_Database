0ef66f7ed1965a1e43e9005ed8825a0d729e6ae8
JULIO SAMPAIO <julio.sampaio@senacsp.edu.br>
2026-05-08 10:01:17 -0300

    conn sql

---
+++ untitled/src/Pck_DAO/ConexaoMySql.java
@@ -0,0 +1,59 @@
+package Pck_DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySql {
+    private String sDriver;
+    private String sServidor;
+    private String sBase;
+
+    private String connStr;
+
+    static Connection conn;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/bd_agenda_mvc";
+    public static final String login = "root";
+    public static final String senha = "";
+
+    public ConexaoMySql(){
+        this.sDriver = "com.mysql.cj.jdbc.Driver";
+        this.sServidor = "localhost";
+        this.connStr = "jdbc:mysql://localhost:3306/bd_agenda_mvc";
+
+    }
+
+    public static Connection getConn(String login, String senha) throws SQLException {
+        conn = null;
+        try{
+            Class.forName(sDriver);
+            conn = DriverManager.getConnection(connStr, login, senha);
+            System.out.println("Conectado!!");
+            return conn;
+
+        } catch (ClassNotFoundException e){
+            String error = "Driver nao encontrado";
+            throw new SQLException(error, e);
+
+        } catch (SQLException e){
+            String error = "Erro ao obter conexao";
+            throw new SQLException(error, e);
+        }
+    }
+
+    public void desconectar(){
+        try{
+            if(conn != null){
+                conn.close();
+                System.out.println("Banco desconectado.");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
